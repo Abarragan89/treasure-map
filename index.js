b5da7bd7a6f798220ec89f0183ff848947ad2710
Anthony Barragan <anthony.bar.89@gmail.com>
2021-12-28 19:05:03 -0800

    Added styles. Working on functionality

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 var getRandomNumber = function(size) {
     return Math.floor(Math.random() * size);
 };
+//Setting up image on Canvas
+
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d');
+var pic = new Image();
+pic.src = "https://m.media-amazon.com/images/I/81jIoI7cbiL._AC_SL1500_.jpg"
+pic.addEventListener('load', function() {ctx.drawImage(pic, 0, 0, canvas.width, canvas.height)}, false)
+
 //calculate distance between treasure and click
 var getDistance = function (event, target) {
     var diffX = event.offsetX - target.x;
@@ -9,22 +17,25 @@ var getDistance = function (event, target) {
     return Math.sqrt((diffX * diffX) + (diffY * diffY));
 };
 
-var width = 750;
-var height = 500;
+var width = canvas.width;
+var height = canvas.height;
 var clicks = 0;
 var clickLimit = 20
 
 //click handler
-$("#map").click(function(event) {
+$("#canvas").click(function(event) {
+    drawStrikes(event)
     clicks++;
     var distance = getDistance(event, target);
     var distanceHint = getDistanceHint(distance);
     $("#distance").text(distanceHint);
-    $("#clickcounter").text("You have " + (clickLimit-clicks) + " clicks left");
-    if (distance < 14 ) {
+    $("#clickcounter").text(clickLimit-clicks);
+    if (distance < 10 ) {
+        drawCircle("#FF0000", "000000");
         alert("You found the treasure in " + clicks + " clicks!");
-    }if (clicks > clickLimit) {
+    }if (clicks >= clickLimit) {
         alert("GAME OVER: You ran out of guesses!");
+        drawCircle("#FF0000", "OOOOOO");
     }
 });
 
@@ -36,18 +47,52 @@ var target = {
 //How close is the player
 var getDistanceHint = function (distance) {
     if (distance < 25) {
-        return "Boiling hot!";
+        return "YOU CAN'T GET ANY CLOSER!!!";
     } else if (distance < 45) {
-        return "Really hot!";
-    } else if (distance < 60) {
-        return "Hot";
-    } else if (distance < 120) {
-        return "Warm";
-    } else if (distance < 180) {
-        return "Cold";
-    } else if (distance < 320) {
-        return "Really Cold";
+        return "Really hot stuff!!";
+    } else if (distance < 160) {
+        return "Hot Stuff!";
+    } else if (distance < 220) {
+        return "getting warmer...";
+    } else if (distance < 280) {
+        return "a little better.";
+    } else if (distance < 420) {
+        return "Not Here.";
     } else {
-        return "Freezing!";
+        return "Freezing.";
     }
 };
+
+
+// functions for marking map. 
+function drawStrikes (event) {
+    // Get coordinates of mouse event relative to canvas
+    const playingArea = document.querySelector('canvas#canvas')
+    let rect = playingArea.getBoundingClientRect();
+    console.log(event);
+    console.log(rect);
+    let x = event.offsetX;
+    let y = event.offsetY;
+    // Draw Circle with X and Y of mouse click
+    ctx.beginPath();
+    ctx.arc(x, y,  10, 0, Math.PI * 2);
+    ctx.fillStyle = '#00FF00'
+    ctx.fill();
+    ctx.strokeStyle = "#000000"
+    ctx.stroke();
+    ctx.closePath();
+    
+}
+
+// DRAW CIRLCE FUNCTION 
+function drawCircle (color, outline) {
+    let x = target.x;
+    let y = target.y;
+    ctx.beginPath();
+    ctx.arc(x, y,  5, 0, Math.PI * 2);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.strokeStyle = outline;
+    ctx.stroke();
+    ctx.closePath();
+}
