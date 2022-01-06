f7adf4ef2a76b3891d3c013b763a4cf45bce3316
Anthony Barragan <anthony.bar.89@gmail.com>
2022-01-05 23:10:37 -0800

    made canvas responsive with accurate clicks when resized

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-//get a random number from 0 to size
-var getRandomNumber = function(size) {
-    return Math.floor(Math.random() * size);
-};
-//Setting up image on Canvas
+// Initial Global Canvas Set Up
 let canvas = document.getElementById('canvas')
 let ctx = canvas.getContext('2d');
 let pic = new Image();
 pic.src = "https://m.media-amazon.com/images/I/81jIoI7cbiL._AC_SL1500_.jpg"
-pic.addEventListener('load', function() {ctx.drawImage(pic, 0, 0, canvas.width, canvas.height)}, false)
+pic.addEventListener('load', function () { ctx.drawImage(pic, 0, 0, canvas.width, canvas.height) }, false);
 
-function canvasSetUp () {
+// Function to make Canvas Responsive
+function initCanvas() {
+    const dynamicWidth = window.innerWidth * 0.63;
+    document.getElementById('canvas').width = Math.floor(dynamicWidth);
+
+    //Setting up image on Canvas
+    canvasSetUp();
+}
+// listn
+window.addEventListener('resize', initCanvas);
+window.addEventListener('load', initCanvas);
+
+//get a random number from 0 to size
+var getRandomNumber = function (size) {
+    return Math.floor(Math.random() * size);
+};
+
+// function to put image on canvas
+function canvasSetUp() {
     let canvas = document.getElementById('canvas')
     let ctx = canvas.getContext('2d');
     let pic = new Image();
     pic.src = "https://m.media-amazon.com/images/I/81jIoI7cbiL._AC_SL1500_.jpg"
-    pic.addEventListener('load', function() {ctx.drawImage(pic, 0, 0, canvas.width, canvas.height)}, false)
+    pic.addEventListener('load', function () { ctx.drawImage(pic, 0, 0, canvas.width, canvas.height) }, false);
 }
-
 
 //calculate distance between treasure and click
 var getDistance = function (event) {
@@ -26,69 +39,72 @@ var getDistance = function (event) {
 };
 //Target Object
 class Target {
-    constructor (clicks, width, height) {
+    constructor(clicks, width, height) {
         this.clicks = clicks;
         this.width = width;
         this.height = height;
         this.x = undefined;
         this.y = undefined;
     }
-    randomSpot () {
+    randomSpot() {
         let x = getRandomNumber(this.width - 10);
         let y = getRandomNumber(this.height - 10);
         this.x = x;
         this.y = y;
     }
-    subtractClick () {
+    subtractClick() {
         this.clicks -= 1;
     }
-    drawCircle () {
+    drawCircle() {
         let x = this.x;
         let y = this.y;
+
+        console.table({ x, y });
+
         ctx.beginPath();
-        ctx.arc(x, y,  5, 0, Math.PI * 2);
+        ctx.arc(x, y, 5, 0, Math.PI * 2);
         ctx.fillStyle = 'red';
         ctx.fill();
         ctx.strokeStyle = 'black';
         ctx.stroke();
         ctx.closePath();
     }
-}; 
+};
 //Canvas Click Event
 // Dig a hole 
-function dig (event) {
+function dig(event) {
     drawStrikes(event)
     target1.subtractClick();
     var distance = getDistance(event);
     var distanceHint = getDistanceHint(distance);
     $("#distance").text(distanceHint);
     $("#clickcounter").text(target1.clicks);
-    
+
     // If you Win!
-    if (distance < 15 ) {
+    if (distance < 15) {
         target1.drawCircle();
         alert("You found the treasure and kept " + target1.clicks + "!!");
         gameEnd();
-    // If you lose.
-    }if (target1.clicks <= 0) {
+        // If you lose.
+    } if (target1.clicks <= 0) {
         target1.drawCircle();
         alert("GAME OVER: You ran out of guesses!");
         gameEnd();
     }
 }
 //When you run out of clicks or win. 
-function gameEnd () {
+function gameEnd() {
     canvas.removeEventListener("click", dig);
-        $("#start-stop").prop("value", "reset");
+    $("#start-stop").prop("value", "reset");
 }
 
 //Start / Stop button 
 // Make New Target Object and Place it.
 $("#start-stop").click(function () {
     // Raise opacity, clear the screen, and spot new target
-    $("#canvas").css("opacity", "1")
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    $("#canvas").css("opacity", "1");
     canvasSetUp();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     target1 = new Target(20, canvas.width, canvas.height);
     target1.randomSpot();
     // Add event listener and display clicks.
@@ -124,16 +140,17 @@ var getDistanceHint = function (distance) {
 };
 
 // functions for marking map. 
-function drawStrikes (event) {
+function drawStrikes(event) {
     // Get coordinates of mouse event relative to canvas
     const playingArea = document.querySelector('canvas#canvas')
-    let rect = playingArea.getBoundingClientRect();
     let x = event.offsetX;
     let y = event.offsetY;
 
+    console.table({ x, y });
+
     // Draw Circle with X and Y of mouse click
     ctx.beginPath();
-    ctx.arc(x, y,  10, 0, Math.PI * 2);
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
     ctx.fillStyle = '#00FF00'
     ctx.fill();
     ctx.strokeStyle = "#000000"
